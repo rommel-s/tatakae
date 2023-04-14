b23dd60fa3b35ea72442fadda95e792d86095158
Rommel Santos <53845558+rommel-s@users.noreply.github.com>
2023-04-14 12:32:41 -0300

    Starting PWa Build and setting manifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Tatakae - Citações de Animes",
+        short_name: "Tatakae",
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#E97600",
+        background_color: "#0F172A",
+        icons: [
+          {
+            src: "/assets/icons/512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/assets/icons/192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/assets/icons/144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
